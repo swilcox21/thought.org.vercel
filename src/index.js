38c8aa10b8aa3748c33a57930a9447c500c82cc0
Sam Wilcox <swilcox21@gmail.com>
2022-12-16 16:16:18 -0500

    im tired

--- src/index.js
+++ src/index.js
@@ -4,16 +4,16 @@ import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
-import reminders from "./reminders/reducer";
-import thots from "./thots/reducer";
+import reminderReducer from "./reminders/reducer";
+import thotReducer from "./thots/reducer";
 import rootReducer from "./reducer";
-export const development = false;
+export const development = true;
 export const dev = "http://localhost:3001/";
 export const prod = "https://thought-org.vercel.app/";
 export const baseURL = "https://thorgapi.herokuapp.com";
 
 export const store = configureStore({
-  reducer: combineReducers({ reminders, rootReducer, thots }),
+  reducer: combineReducers({ reminderReducer, rootReducer, thotReducer }),
 });
 
 ReactDOM.render(
